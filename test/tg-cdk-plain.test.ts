717d6388a771affca3692cabab888bb01dd1339c
Nikit Zykov <nikit@zykov.com>
2024-07-19 17:29:20 +0200

    Initial commit

---
+++ test/tg-cdk-plain.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TgCdkPlain from '../lib/tg-cdk-plain-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tg-cdk-plain-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TgCdkPlain.TgCdkPlainStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
